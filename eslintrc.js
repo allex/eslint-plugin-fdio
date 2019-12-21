b57d62fa70b664c8fd71cce72fc593c24b3dceb3
Allex Wang <allex.wxn@gmail.com>
2019-12-21 16:14:53 +0800

    chore: disable vue/invalid-first-character-of-tag-name

--- eslintrc.js
+++ eslintrc.js
@@ -129,6 +129,9 @@ module.exports = {
     'vue/max-attributes-per-line': 0,
     'vue/html-self-closing': 0,
     'vue/singleline-html-element-content-newline': 0,
-    'vue/mustache-interpolation-spacing': 0
+    'vue/mustache-interpolation-spacing': 0,
+    'vue/no-parsing-error': [2, {
+      'invalid-first-character-of-tag-name': false
+    }]
   }
 }
