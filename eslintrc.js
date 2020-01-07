405ecde453803646004401285cdca0e5ffa23875
Allex Wang <allex.wxn@gmail.com>
2020-01-07 17:45:55 +0800

    feat: add typescript rules

--- eslintrc.js
+++ eslintrc.js
@@ -1,11 +1,27 @@
 var allExtensions = ['.js', '.jsx', '.ts', '.tsx', '.d.ts', '.vue']
 
 module.exports = {
+  // Prerequisite `eslint-plugin-vue`, being extended, sets
+  // root property `parser` to `'vue-eslint-parser'`, which, for code parsing,
+  // in turn delegates to the parser, specified in `parserOptions.parser`:
+  // https://github.com/vuejs/eslint-plugin-vue#what-is-the-use-the-latest-vue-eslint-parser-error
+  parserOptions: {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    parser: '@typescript-eslint/parser'
+  },
   plugins: [
     '@typescript-eslint',
     'import'
   ],
   extends: [
+    'eslint:recommended',
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/src/configs
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/recommended',
     'plugin:import/typescript'
   ],
@@ -32,14 +48,6 @@ module.exports = {
     'document': false,
     'navigator': false,
     'window': false
-  },
-  parserOptions: {
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'jsx': true
-    },
-    'parser': '@typescript-eslint/parser'
   },
   rules: {
     'arrow-parens': [2, 'as-needed'],
@@ -69,12 +77,6 @@ module.exports = {
         ['in', 'instanceof']
       ],
       'allowSamePrecedence': true
-    }],
-    'no-unused-vars': [2, {
-        'vars': 'local',
-        'args': 'none',
-        'caughtErrors': 'none',
-        'ignoreRestSiblings': true
     }],
     'no-cond-assign': 0,
     'no-floating-decimal': 0,
@@ -115,10 +117,36 @@ module.exports = {
 
     'quotes': [2, 'single', { 'allowTemplateLiterals': true, 'avoidEscape': true }],
 
+    // note you must disable the base rule as it can report incorrect errors
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [1, {
+      'vars': 'local',
+      'args': 'none',
+      'ignoreRestSiblings': true,
+      'caughtErrors': 'none'
+    }],
+    '@typescript-eslint/explicit-function-return-type': 1,
+    '@typescript-eslint/no-this-alias': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-inferrable-types': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/member-delimiter-style': [2, {
+      'multiline': {
+        'delimiter': 'semi',
+        'requireLast': true
+      },
+      'singleline': {
+        'delimiter': 'semi',
+        'requireLast': true
+      }
+    }],
+    '@typescript-eslint/interface-name-prefix': 0,
+
     'import/no-unresolved': 0,
     'import/no-named-default': 1,
     'import/prefer-default-export': 0,
-    "import/newline-after-import": 2,
+    'import/newline-after-import': 2,
     'import/extensions': [
       2,
       'ignorePackages',
